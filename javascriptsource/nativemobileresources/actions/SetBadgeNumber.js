5cce4fdee345866c2dd2574cb1f415a02bd9266e
Jorge Flores <jorge@entidad.io>
2023-08-07 21:10:20 -0700

    Initialzed repo with module Aries_SDK exported from Farmworker WalletOS source repository
    Signed-off-by: Jorge Flores <jorge@entidad.io>

---
+++ javascriptsource/nativemobileresources/actions/SetBadgeNumber.js
@@ -0,0 +1,27 @@
+import { Platform, NativeModules } from 'react-native';
+import PushNotification from 'react-native-push-notification';
+
+// BEGIN EXTRA CODE
+// END EXTRA CODE
+/**
+ * @param {Big} badgeNumber - This field is required. Should be greater than or equal to 0.
+ * @returns {Promise.<void>}
+ */
+async function SetBadgeNumber(badgeNumber) {
+    // BEGIN USER CODE
+    // Documentation https://github.com/zo0r/react-native-push-notification
+    const isIOS = Platform.OS === "ios";
+    if (NativeModules && isIOS && !NativeModules.RNCPushNotificationIOS) {
+        return Promise.reject(new Error("Notifications module is not available in your app"));
+    }
+    if (!badgeNumber) {
+        return Promise.reject(new Error("Input parameter 'Badge number' is required"));
+    }
+    if (badgeNumber.lt(0)) {
+        return Promise.reject(new Error("Input parameter 'Badge number' should be zero or greater"));
+    }
+    return PushNotification.setApplicationIconBadgeNumber(Number(badgeNumber));
+    // END USER CODE
+}
+
+export { SetBadgeNumber };
---
+++ javascriptsource/nativemobileresources/actions/Vibrate.js
@@ -0,0 +1,18 @@
+import { Vibration } from 'react-native';
+
+// BEGIN EXTRA CODE
+// END EXTRA CODE
+/**
+ * @param {Big} duration - Android only setting. The time (in milliseconds) the device should vibrate. Set to empty to use the default value 500.
+ * @returns {Promise.<void>}
+ */
+async function Vibrate(duration) {
+    // BEGIN USER CODE
+    // Documentation https://facebook.github.io/react-native/docs/vibration#vibrate
+    const pattern = duration ? Number(duration) : 500;
+    Vibration.vibrate(pattern, false);
+    return Promise.resolve();
+    // END USER CODE
+}
+
+export { Vibrate };
